c5dd35f3c13f242b30bb2a92db3f4823c9ff57e4
marko <marko@dhcp-10-9-32-126.kotnet.kuleuven.be>
2009-09-09 15:39:02 +0200

    some debugging

--- src/chameleon/editor/presentation/OutlineComparator.java
+++ src/chameleon/editor/presentation/OutlineComparator.java
@@ -8,14 +8,23 @@ import chameleon.core.element.Element;
 import chameleon.core.language.Language;
 import chameleon.editor.connector.EclipseEditorExtension;
 
+/**
+ * A class for comparing objects in order to sort them in an Eclipse view. The comparator
+ * uses the Eclipse mechanism of assigning a category to different kinds of elements.
+ * The super class uses alphabetical sorting of the lowercase value return by the label provider
+ * to sort elements within a category.
+ * 
+ * If you want a completely different sorting, you must directly extend ViewerComparator and override the compare
+ * method.
+ * 
+ * The class wrap a DeclarationCategorizer because otherwise, the connector classes for language modules depend on
+ * Eclipse classes, resulting in classpath problems.
+ * 
+ * @author Marko van Dooren
+ */
 public class OutlineComparator extends ViewerComparator {
 	
 	public OutlineComparator() {
-	}
-	
-	@Override
-	public int compare(Viewer viewer, Object e1, Object e2) {
-		return super.compare(viewer, e1, e2);
 	}
 	
 	/**
