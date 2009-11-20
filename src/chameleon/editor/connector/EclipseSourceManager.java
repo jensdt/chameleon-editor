6a4720e1632a18f641e78e5c4f63206fa6e062c9
marko <marko@objectron.dyn.cs.kuleuven.be>
2009-11-20 15:35:55 +0100

    deal with absence of location information for reparse

--- src/chameleon/editor/connector/EclipseSourceManager.java
+++ src/chameleon/editor/connector/EclipseSourceManager.java
@@ -2,10 +2,13 @@ package chameleon.editor.connector;
 
 import org.eclipse.jface.text.BadLocationException;
 
+import chameleon.core.compilationunit.CompilationUnit;
 import chameleon.core.element.ChameleonProgrammerException;
 import chameleon.core.element.Element;
 import chameleon.editor.editors.ChameleonDocument;
 import chameleon.editor.project.ChameleonProjectNature;
+import chameleon.input.NoLocationException;
+import chameleon.input.ParseException;
 import chameleon.input.SourceManager;
 import chameleon.tool.Connector;
 import chameleon.tool.ConnectorImpl;
@@ -32,11 +35,14 @@ public class EclipseSourceManager extends ConnectorImpl implements SourceManager
 		return new EclipseSourceManager(null);
 	}
 
-	public String text(Element element) {
-		EclipseEditorTag decorator = (EclipseEditorTag) element.tag(EclipseEditorTag.ALL_TAG);
+	public String text(Element element) throws NoLocationException {
+		EclipseEditorTag location = (EclipseEditorTag) element.tag(EclipseEditorTag.ALL_TAG);
+		if(location == null) {
+			throw new NoLocationException(element);
+		}
 		ChameleonDocument document = projectNature().document(element);
 		try {
-			return document.get(decorator.getOffset(), decorator.getLength());
+			return document.get(location.getOffset(), location.getLength());
 		} catch (BadLocationException e) {
 			throw new ChameleonProgrammerException("Bad location",e);
 		}
